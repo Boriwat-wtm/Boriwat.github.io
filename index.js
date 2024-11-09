9c9f1c611b27ada312dddbe281769002ea640fa8
Boriwat Wongtama <66025740@up.ac.th>
2024-11-09 22:43:31 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+<body>
+    <h1>ยินดีต้อนรับสู่เว็บไซต์ของฉัน</h1>
+    <button id="myButton">Click me</button>
+
+    <!-- เชื่อมต่อไฟล์ JavaScript -->
+    <script src="script.js"></script>
+</body>
+</html>
